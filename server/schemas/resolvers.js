c0962fe57140b6ce523564d57f0a83eb08b01565
Amanda Hoshor <hoshor.manda@gmail.com>
2021-08-04 21:30:41 -0600

    schema files

---
+++ server/schemas/resolvers.js
@@ -0,0 +1,47 @@
+const { User } = require('../models');
+const { AuthenticationError } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+                const userData = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userData;
+            }
+            throw new AuthenticationError('Please log in');
+        }
+    },
+    Mutation: {
+        login: async(parent, {email, password }) => {
+            const user = await User.findOne( { email });
+            if (!user) {
+                throw new AuthenticationError('Incorrect info')
+            }
+            const correctPw = await user.isCorrectPassword(password);
+            if(!correctPw) {
+                throw new AuthenticationError('Incorrect info')
+            }
+            const token = signToken(user);
+            return { token, user };
+        },
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user)
+            return { token, user };
+        },
+        saveBook: async(parent, { book }, context) => {
+            if (context.user) {
+                const updatedUser = await User.findOneAndUpdate(
+                    {_id: context.user._id},
+                    { $pull: { savedBooks: { bookId: bookId }}},
+                    { new: true }
+                )
+                return updatedUser;
+            }
+        }
+    }
+}
+
+module.exports = resolvers;
