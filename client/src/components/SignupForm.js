48a523c53314be28564badb9956ee1149fc53a2e
Amanda Hoshor <hoshor.manda@gmail.com>
2021-08-07 23:08:50 -0600

    update savedBooks.js

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -2,8 +2,9 @@ import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
 import { useMutation } from '@apollo/react-hooks';
-import Auth from '../utils/auth';
 import { ADD_USER } from '../utils/mutations';
+import Auth from '../utils/auth';
+
 
 const SignupForm = () => {
   // set initial form state
