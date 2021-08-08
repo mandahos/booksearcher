6717381d4fca3dd36dc6416a031fdc7a931772a0
Amanda Hoshor <hoshor.manda@gmail.com>
2021-08-07 23:53:37 -0600

    trying to fix error

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
+import Auth from '../utils/auth';
+
 import { useMutation } from '@apollo/react-hooks';
 import { ADD_USER } from '../utils/mutations';
-import Auth from '../utils/auth';
+
 
 
 const SignupForm = () => {
